7600ffe940e755ebf63aa480099f40ee0354e4c5
sakkke <w32w64@gmail.com>
2022-09-17 04:20:59 +0000

    feat: pass tests

--- muos/get_nth_partition.py
+++ muos/get_nth_partition.py
@@ -1,2 +1,11 @@
 def get_nth_partition(disk: str, n: int) -> str:
+    if disk.startswith('/dev/loop'):
+        return '{}p{}'.format(disk, n)
+
+    if disk.startswith('/dev/mmcblk'):
+        return '{}p{}'.format(disk, n)
+
+    if disk.startswith('/dev/nvme'):
+        return '{}p{}'.format(disk, n)
+
     return '{}{}'.format(disk, n)
