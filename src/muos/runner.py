490692d41f2df1048799497f8d6cce12e3329c81
sakkke <w32w64@gmail.com>
2022-09-19 02:39:29 +0000

    docs: add comments

--- src/muos/runner.py
+++ src/muos/runner.py
@@ -5,13 +5,28 @@ from .log import Log
 from .step import Step
 
 class Runner:
+    '''A class to run steps'''
+
     environment: Environment
+    '''Environment of Runner
+
+    Example: ``Environment()``
+    '''
 
     def __init__(self, environment: Environment, steps: List[Step]) -> None:
+        '''Initialize Runner
+
+        Args:
+            environment (Environment): A value of ``Environment`` type.
+            steps (List[Step]): ``List`` of ``Step`` type.
+        '''
+
         self.environment = environment
         self.steps = steps
 
     def run(self) -> None:
+        '''A function to run steps'''
+
         Log.message(self.environment.name)
 
         for step in self.steps:
